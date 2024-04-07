d728ceb45324fdd0cfa1be14371df54db828fbe3
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-04-07 16:23:19 +0600

    Implement register_type class

---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef RED_BALCONY_REGISTER_TYPES_H
+#define RED_BALCONY_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_module(ModuleInitializationLevel p_level);
+void uninitialize_module(ModuleInitializationLevel p_level);
+
+#endif // RED_BALCONY_REGISTER_TYPES_H
